718212f88c823f02d16f5146ecd17b356de3aa06
Gustavo Lopez <gustavo@getkalto.com>
2023-01-22 13:57:04 -0300

    Setup of the eslint-config-prettier

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -5,31 +5,40 @@ import { MOCK_PROJECTS } from './MockProjects';
 type Props = {};
 
 const ProjectsPage = (props: Props) => {
-  const names = {
-    name: 'John',
-    age: 30,
-    car: null,
-  };
+	const names = {
+		name: 'John',
+		age: 30,
+		car: null,
+	};
 
-  const arrayTest = [
-    {
-      name: 'John',
-      age: 34,
-      car: null,
-    },
-    {
-      name: 'Josua',
-      age: 21,
-      car: null,
-    },
-  ];
+	const a = [1, 2, 3];
+	const arrayTest = [
+		{
+			name: 'John',
+			age: 34,
+			car: null,
+		},
+		{
+			name: 'Josua',
+			age: 21,
+			car: null,
+		},
+	];
 
-  return (
-    <>
-      <Header />
-      <pre>{JSON.stringify(MOCK_PROJECTS, null, 2)}</pre>
-      <Footer />
-    </>
-  );
+	if (a.length > 0) {
+		console.log('a is not empty');
+	}
+
+	const fnTest = (a: number) => {
+		return a + 9;
+	};
+
+	return (
+		<>
+			<Header />
+			<pre>{JSON.stringify(MOCK_PROJECTS, null, 2)}</pre>
+			<Footer />
+		</>
+	);
 };
 export default ProjectsPage;
